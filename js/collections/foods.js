8e825c6b8c6feb59cbe0d0fac3121b9c2dec2f40
Matthew Nerger <mnerger@lsu.edu>
2016-05-04 17:43:04 -0500

    Create foods collection

--- js/collections/foods.js
+++ js/collections/foods.js
@@ -1,1 +1,17 @@
 var app = app || {};
+
+(function() {
+  'use strict';
+
+  var Foods = Backbone.Collection.extend({
+    model: app.Todo,
+
+    localStorage: new Backbone.LocalStorage('calorie-tracker-backbone'),
+
+    // TODO: Add functions, if needed
+
+    comparator: 'order'
+  });
+
+  app.foods = new Foods();
+})();
