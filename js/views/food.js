20515dce892d24b40c6902d293b10a90592ae19d
Matthew Nerger <mnerger@lsu.edu>
2016-05-04 18:12:02 -0500

    Add food view

--- js/views/food.js
+++ js/views/food.js
@@ -6,6 +6,29 @@ var app = app || {};
   app.FoodView = Backbone.View.extend({
     tagName: 'li',
 
-    
+    template: _.template($('#food-template').html()),
+
+    events: {
+      'click .delete-button': 'clear'
+    },
+
+    initialize: function() {
+      this.listenTo(this.model, 'change', this.render);
+      this.listenTo(this.model, 'destroy', this.remove);
+    },
+
+    render: function() {
+      // bug work around
+      if (this.model.changed.id !== undefined) {
+				return;
+			}
+
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    },
+
+    clear: function () {
+			this.model.destroy();
+		}
   });
-})();
+})(jQuery);
